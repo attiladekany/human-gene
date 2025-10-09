1e549480a01fe9f77e9d1c2d8f7cd31b667509a7
attiladekany <dekanyattila94@gmail.com>
2025-10-09 19:59:30 +0200

    feat: add typescript, sass support

--- frontend/react-genes-app/src/App.tsx
+++ frontend/react-genes-app/src/App.tsx
@@ -1,10 +1,12 @@
-import './App.css';
+import './App.scss';
+import Table from './Table';
 
 const App = () => {
   return (
     <div className="content">
       <h1>Rsbuild with React</h1>
       <p>Start building amazing things with Rsbuild.</p>
+      <Table />
     </div>
   );
 };
---
+++ frontend/react-genes-app/src/custom.d.ts
@@ -0,0 +1,16 @@
+declare module '*.scss' {
+  const content: { [className: string]: string };
+  export default content;
+}
+declare module '*.sass' {
+  const content: { [className: string]: string };
+  export default content;
+}
+declare module '*.module.scss' {
+  const content: { [className: string]: string };
+  export default content;
+}
+declare module '*.module.sass' {
+  const content: { [className: string]: string };
+  export default content;
+}
